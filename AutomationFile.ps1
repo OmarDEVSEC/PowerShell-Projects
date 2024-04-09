441fb2df21f8136a62787e8b615e9d10432df7b5
Omar DevSec <omarsecure30@gmail.com>
2024-04-09 15:44:57 -0700

    Created varaibles for destination and source, also done with the initial if statment that creates dirctory if one does not exist

--- AutomationFile.ps1
+++ AutomationFile.ps1
@@ -1,1 +1,13 @@
-Write-Host "My first script. It Is just a test, thanks I will work hard to complete this automation project."
+#Define the source directory where the PDF files are located
+$sourceDirectory = "C:\Users\homar\OneDrive\Desktop\"
+
+#Defining the destination of the directory 
+$destinationDirectory = "C:\Users\homar\OneDrive\Desktop\PDFs"
+
+#creating the does not exist
+if (-not (Test-Path -Path $destinationDirectory)) {
+    New-Item -Path $destinationDirectory -ItemType Directory
+}
+
+Write-Host 
+"My first script. It Is just a test, thanks I will work hard to complete this automation project."
