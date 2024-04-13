8abc308757d941a8e35a72a7d4ae3bc7d33f078d
Omar DevSec <omarsecure30@gmail.com>
2024-04-13 11:54:10 -0700

    Defining CPU, DISK, AND STORAGE Thresholds

---
+++ AutomationFile.ps1
@@ -0,0 +1,7 @@
+#Define the source directory where the PDF files are located
+$sourceDirectory = 
+
+
+
+Write-Host 
+"My first script. It Is just a test, thanks I will work hard to complete this automation project."
