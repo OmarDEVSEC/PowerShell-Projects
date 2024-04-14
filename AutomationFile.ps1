7a9a36df521eac682204b04e2e0106315c8bd1db
Omar DevSec <omarsecure30@gmail.com>
2024-04-13 21:49:05 -0700

    Defining Variables for the CPU, Memory, and Disk

--- AutomationFile.ps1
+++ AutomationFile.ps1
@@ -1,7 +1,22 @@
 #Define the source directory where the PDF files are located
-$sourceDirectory = 
+$sourceDirectory = "C:\Users\homar\OneDrive\Desktop\"
 
+#Defining the destination of the directory 
+$destinationDirectory = "C:\Users\homar\OneDrive\Desktop\PDFs"
 
+#creating the destination folder if one does not exist
+if (-not (Test-Path -Path $destinationDirectory)) {
+    New-Item -Path $destinationDirectory -ItemType Directory
+}
+
+#Get all the PDF files from the source directory
+$pdfFiles = Get-Childitem -Path $sourceDirectory -Filter "*.pdf" -File
+
+#Move each PDF file to the destination directory
+foreach ($file in $pdfFiles) {
+    $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $file.Name 
+    Move-item -Path $file.FullName -Destination $destinationPath -Force
+}
 
 Write-Host 
-"My first script. It Is just a test, thanks I will work hard to complete this automation project."
+"You have succesfuly moved the PDF files to folder PDFs"
---
+++ SystemMonitoring.ps1
@@ -0,0 +1,9 @@
+#Script To monitor system CPU usage, memory usage, disk space, and services status
+
+#Definig CPU, Memory, and disk thresholds
+$cpuThreshold = 80
+$memoryThreshold =  50
+$diskThreshold = 10
+ 
+
+Write-host "Hello World"
