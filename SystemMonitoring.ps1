1f007abde4b9d046dcca000c67211cdea5740ac5
OmarDEVSEC <166287080+OmarDEVSEC@users.noreply.github.com>
2024-04-13 22:11:21 -0700

    Define the variables to store thesholds for CPU, DISk, And Memory. Variable and code to check memory and CPU usage

--- SystemMonitoring.ps1
+++ SystemMonitoring.ps1
@@ -1,7 +1,15 @@
-#Script To monitor system CPU usage, memory usage, disk space, and services status
-$monitorCPU
-$monitorUsage
-$monitorSpace
-$montorStatus 
+#Script To monitor system CPU usage, memory usage, disk space, 
 
-Write-host "Hello World"
+# Define the variables to store thesholds for CPU, DISk, And Memory
+
+$cpuThreshold = 80
+$memoryThreshold = 70 
+$spaceThreshold = 50
+
+#Variable and code to check CPU Usage
+
+$cpuUsage =  (Get-counter '/Processor(_Total)\% Processor Time').CounterSamples.CookedValue
+
+#Variable and code to check memory usage
+
+$memoryUsage = (Get-Counter '\Memory\% Committed Bytes In Use').CounterSamples.CookedValue
