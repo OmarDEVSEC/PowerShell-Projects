8ff298abd514238300a1f0c02e761ffc108a6fa0
Omar DevSec <omarsecure30@gmail.com>
2025-03-02 16:49:25 -0800

    Completedv heck diskspac and diskusgae snippet included comments and math functions to calculae

--- SystemMonitoring.ps1
+++ SystemMonitoring.ps1
@@ -11,5 +11,17 @@ $spaceThreshold = 50 #The capacity that is going to trigger an output of storage
 $cpuUsage =  (Get-counter '/Processor(_Total)\% Processor Time').CounterSamples.CookedValue
 
 #Variable and code to check memory usage
-$memory = Get-CimInstance 
+$memory = Get-CimInstance Win32_OperatingSystem
 $memoryUsage = (Get-Counter '\Memory\% Committed Bytes In Use').CounterSamples.CookedValue
+
+#Check disk space using disk and diskusage
+$disk = Get-PSDrive -PSProvider FileSystem | Where-Object {$_.Used -or $_.Free}
+$diskUsage = $disk | ForEach-Object{
+    [PSCustomObject]@{
+        Drive = $_.Name #Drive letter (e.g. C,D,E)
+        FreeSpaceGb = [math]::Round($_.Free / 1GB, 2) #Convert free space to GB with 2 decimal percision
+        TotalSpaceGB = [math]::Round($_.Used + $_.Free/ 1GB, 2) #Calculate the total disk space
+        UsedPercentage = [math]::Round((($_.Used) / ($_.Used + $_.Free) * 100), 2) #Calculate percentage of used space
+
+    }
+}
