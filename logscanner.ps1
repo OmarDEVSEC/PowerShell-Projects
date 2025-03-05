506cca456017145657658ae7d5c4288bdb937d84
Omar DevSec <omarsecure30@gmail.com>
2025-03-05 11:03:13 -0800

    Defining script parameters, search for log path, search for patterns in logs

--- logscanner.ps1
+++ logscanner.ps1
@@ -4,4 +4,22 @@
 #outputs findings to a CSV file
 # Run script with a command .\logscanner.ps1 -LogPath "C:\Logs"
 
+#defining script parameters
+param(
+    #Parameter contianing path to the directory containing log files
+    [Parameter(Mandatory=$true)]
+    [string]$LogPath,
+
+    #file path for the CSV output including timestamp -default name
+    # Parameter help description
+    [Parameter(Mandatory=$false)]
+    [String]$OutputFile = "SecurityIncidents_$(Get-Date -Format 'yyyyMMdd').CSV",
+
+    #Array of patterns to search in logs pulled
+    [Parameter(Mandatory=$false)]
+    [string[]]$Patterns = @(
+        "failed loging","authentication failure","access denied",
+        "firewall block","malware","suspicious", "brute force"    
+        )
+)
 
