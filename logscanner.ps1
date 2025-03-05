32bfed2ba07c6481d2468c55e97c9a9303c64585
Omar DevSec <omarsecure30@gmail.com>
2025-03-04 19:31:41 -0800

    Instructions on purpose and utilization of script

---
+++ logscanner.ps1
@@ -0,0 +1,7 @@
+#Log scanner script. Best use cases: Takes parameters for log directory path,
+#outputfile, and search patterns
+# Scans text-based log files (*.log *.txt), searches for common security incident indicators
+#outputs findings to a CSV file
+# Run script with a command .\logscanner.ps1 -LogPath "C:\Logs"
+
+
